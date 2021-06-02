df4e4b9453f05f69beaaf11d33604bddfae407ad
Henrique de Campos Duller <henriqueduller@hotmail.com>
2021-06-02 16:42:13 -0300

    itoa - putchar - putendl - putnbr - putstr - strmapi. Norminette v3.0 OK, warmachine OK

--- libft.h
+++ libft.h
@@ -2,6 +2,7 @@
 # define LIBFT_H
 # include <stddef.h>
 # include <stdlib.h>
+# include <unistd.h>
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
@@ -31,5 +32,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
