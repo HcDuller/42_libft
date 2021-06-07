4245bf2aec6e0823620cf91bb2b518b5b1935cab
Henrique de Campos Duller <henriqueduller@hotmail.com>
2021-06-07 16:36:07 -0300

    ft_lstclear changes 10

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: hcduller <hcduller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 16:39:39 by hcduller          #+#    #+#             */
-/*   Updated: 2021/06/07 16:34:16 by hcduller         ###   ########.fr       */
+/*   Updated: 2021/06/07 16:36:01 by hcduller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 	while (p)
 	{
 		a = p->next;
-		del(p);
+		ft_lstdelone(p, del);
 		free(p);		
 		p = a;
 	}
