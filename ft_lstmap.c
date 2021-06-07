d0d6edffb9fe77aa3c2e774717db9da85df5b62e
Henrique de Campos Duller <henriqueduller@hotmail.com>
2021-06-07 14:59:02 -0300

    Bonus warmachine / Norminette v3.0 compliant (Y)

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,33 +6,60 @@
 /*   By: hcduller <hcduller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 17:25:05 by hcduller          #+#    #+#             */
-/*   Updated: 2021/06/04 18:02:07 by hcduller         ###   ########.fr       */
+/*   Updated: 2021/06/07 14:58:06 by hcduller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static	int	ft_mapper(t_list **al, t_list *or, void *(*f)(void *), int len);
+static	void	freer(t_list **al);
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*p;
 	t_list	**t;
 	int		l;
-	int		i;
 
 	if (!lst || !f || !del)
 		return (NULL);
-	p = lst;
 	l = ft_lstsize(lst);
-	i = 0;
-	t = (t_list **)malloc(l * sizeof (void *));
+	t = (t_list **)ft_calloc(l + 1, sizeof (void *));
 	if (t)
 	{
-		while (i < l)
+		if (!ft_mapper(t, lst, f, l))
 		{
-			t[i++] = f(p);
-			p = p->next;
+			freer(t);
+			return (NULL);
 		}
-		return (*t);
+		p = *t;
+		free(t);
+		return (p);
 	}
 	return (NULL);
+}
+
+int	ft_mapper(t_list **al, t_list *or, void *(*f)(void *), int len)
+{
+	int	i;
+
+	i = 0;
+	while (i < len)
+	{
+		al[i] = ft_lstnew(f(or->content));
+		if (!al[i])
+			return (0);
+		if (i > 0)
+			al[i - 1]->next = al[i];
+		i++;
+		or = or->next;
+	}
+	return (1);
+}
+
+void	freer(t_list **al)
+{
+	while (*al)
+		free(*al++);
+	free(al);
 }
