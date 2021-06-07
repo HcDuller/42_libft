92e06abda711b0a48ade199e752efe6f717c8a61
Henrique de Campos Duller <henriqueduller@hotmail.com>
2021-06-07 15:14:44 -0300

    ft_lstadd_front changes

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: hcduller <hcduller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 15:02:00 by hcduller          #+#    #+#             */
-/*   Updated: 2021/06/04 15:44:37 by hcduller         ###   ########.fr       */
+/*   Updated: 2021/06/07 15:14:12 by hcduller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	if (!lst || !new)
 		return ;
 	new->next = *lst;
-	*(lst + 1) = new;
+	while (*lst)
+		lst++;
+	*(lst) = new;
 }
--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: hcduller <hcduller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/03 21:34:38 by hcduller          #+#    #+#             */
-/*   Updated: 2021/06/07 15:08:22 by hcduller         ###   ########.fr       */
+/*   Updated: 2021/06/07 15:09:49 by hcduller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*p;
 
-	//if (!content)
-	//	return (NULL);
 	p = malloc(sizeof(t_list));
 	if (p)
 	{
