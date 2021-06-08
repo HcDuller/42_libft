2133ab368619c3dd6c3e355a092f6b5d4e40a29d
Henrique de Campos Duller <henriqueduller@hotmail.com>
2021-06-07 23:16:38 -0300

    ft_strrchr 1

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: hcduller <hcduller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 22:01:07 by hde-camp          #+#    #+#             */
-/*   Updated: 2021/05/26 21:34:57 by hcduller         ###   ########.fr       */
+/*   Updated: 2021/06/07 23:16:22 by hcduller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,11 @@ char	*ft_strrchr(const char *s, int c)
 	}
 	while (i >= 0)
 	{
-		if (*r == c)
+		if (*r == (char)c)
 			return (r);
 		r--;
 		i--;
-		if (i == 0 && !(*r == c))
+		if (i == 0 && !(*r == (char)c))
 			return (0);
 	}
 	return (0);
