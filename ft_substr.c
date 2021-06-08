e4c6fadf7e733b3ebc8857092e220c43260c37af
Henrique de Campos Duller <henriqueduller@hotmail.com>
2021-06-08 15:55:18 -0300

    pre entrega 2

--- ft_substr.c
+++ ft_substr.c
@@ -6,46 +6,36 @@
 /*   By: hcduller <hcduller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 15:23:18 by hcduller          #+#    #+#             */
-/*   Updated: 2021/06/08 15:42:16 by hcduller         ###   ########.fr       */
+/*   Updated: 2021/06/08 15:54:58 by hcduller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"libft.h"
 
-static	size_t	smaller(size_t a, size_t b);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	sl;
 	size_t	i;
 	size_t	nl;
-	char	*r;
+	size_t	sl;
+	char	*ptr;
 
-	if (!*s)
+	if (!s)
 		return (NULL);
-	if (!len)
-		return ((char *)ft_calloc(sizeof(char), 1));
-	sl = ft_strlen(s);
 	i = 0;
-	nl = 1;
-	if (sl > start)
-		nl += smaller(len, (sl - start));
-	r = (char *)ft_calloc(sizeof(char), nl);
-	if (r)
-	{
-		while (*(s + start + i) && i + 1 < nl)
+	sl = ft_strlen(s);
+	if (sl < len)
+		nl = sl;
+	else
+		nl = len;
+	ptr = ft_calloc(nl + 1, sizeof(char));
+	if (ptr && start < sl)
+	{		
+		while (i < len && s[start + i])
 		{
-			*(r + i) = *(s + start + i);
+			*(ptr + i) = s[start + i];
 			i++;
 		}
-		*(r + i) = 0;
-	}		
-	return (r);
-}
-
-size_t	smaller(size_t a, size_t b)
-{
-	if (a < b)
-		return (a);
-	return (b);
+	}
+	return (ptr);
 }
