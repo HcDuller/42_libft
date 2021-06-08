90e01ba8109c2dc5b128bc1199adbe9aaa8ee787
Henrique de Campos Duller <henriqueduller@hotmail.com>
2021-06-08 11:23:47 -0300

    Fix ft_strlcpy.c (now, norm compliant)

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: hcduller <hcduller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/13 15:57:39 by hde-camp          #+#    #+#             */
-/*   Updated: 2021/06/08 11:06:24 by hcduller         ###   ########.fr       */
+/*   Updated: 2021/06/08 11:23:35 by hcduller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,18 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	l;
 
 	if (!src || !dst)
-		return (0);	
+		return (0);
 	r = ft_strlen(src);
 	if (dstsize)
 	{
-		l = smaller(r,dstsize - 1);
+		l = smaller(r, dstsize - 1);
 		i = 0;
 		while (i < l)
 		{
 			dst[i] = src[i];
 			i++;
 		}
-		dst[i] = 0;	
+		dst[i] = 0;
 	}
 	return (r);
 }
