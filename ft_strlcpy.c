13b17e387b637b8a29b14088039146a8a66ab764
Henrique de Campos Duller <henriqueduller@hotmail.com>
2021-06-08 11:22:52 -0300

    Fix ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,32 +6,40 @@
 /*   By: hcduller <hcduller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/13 15:57:39 by hde-camp          #+#    #+#             */
-/*   Updated: 2021/06/07 23:37:39 by hcduller         ###   ########.fr       */
+/*   Updated: 2021/06/08 11:06:24 by hcduller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"libft.h"
 
+static	size_t	smaller(size_t a, size_t b);
+
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	r;
+	size_t	l;
 
-	if (!src || !dst || !dstsize)
-		return (0);
+	if (!src || !dst)
+		return (0);	
 	r = ft_strlen(src);
-	i = 0;
-	if(dstsize == 1)
+	if (dstsize)
 	{
-		dst[0] = 0;
-		return (r);
-	}
-	while (i + 1 < dstsize && i < r + 1)
-	{
-		dst[i] = src[i];
-		i++;
-		if (i + 1 == dstsize || i == r)
-			dst[i] = 0;
+		l = smaller(r,dstsize - 1);
+		i = 0;
+		while (i < l)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = 0;	
 	}
 	return (r);
+}
+
+static	size_t	smaller(size_t a, size_t b)
+{
+	if (a < b)
+		return (a);
+	return (b);
 }
