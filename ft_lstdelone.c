00fb7da78f8ca4c336a4d06c5766f9c942d88bd0
Henrique de Campos Duller <henriqueduller@hotmail.com>
2021-06-08 21:17:36 -0300

    pre entrega 8

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: hcduller <hcduller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 16:30:53 by hcduller          #+#    #+#             */
-/*   Updated: 2021/06/07 15:23:34 by hcduller         ###   ########.fr       */
+/*   Updated: 2021/06/08 21:17:29 by hcduller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
 	if (!lst || !del)
 		return ;
-	del(lst);
+	del(lst->content);
 }
