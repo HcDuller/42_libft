9ffdc3796af1a94273e354575689e82a9895ddb2
Henrique de Campos Duller <henriqueduller@hotmail.com>
2021-06-08 21:18:45 -0300

    Ajustes lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: hcduller <hcduller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 16:30:53 by hcduller          #+#    #+#             */
-/*   Updated: 2021/06/08 21:17:29 by hcduller         ###   ########.fr       */
+/*   Updated: 2021/06/08 21:18:31 by hcduller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,5 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*))
 	if (!lst || !del)
 		return ;
 	del(lst->content);
+	free(lst);
 }
