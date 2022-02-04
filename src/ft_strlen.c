9e4c3a3e9dcb3ee0e79deff84e470de12df821b7
Henrique de Campos Duller <henriqueduller@hotmail.com>
2022-02-04 13:52:35 -0300

    Protected ft_strlen against NULL as param

--- src/ft_strlen.c
+++ src/ft_strlen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hcduller <hcduller@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hde-camp <hde-camp@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/13 13:21:33 by hde-camp          #+#    #+#             */
-/*   Updated: 2021/05/26 21:34:22 by hcduller         ###   ########.fr       */
+/*   Updated: 2022/02/04 13:52:01 by hde-camp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@ size_t	ft_strlen(const char *s)
 	size_t	i;
 
 	i = 0;
-	while (s[i] && s[i] != '\0')
+	if (s)
 	{
-		i++;
+		while (s[i] && s[i] != '\0')
+		{
+			i++;
+		}
 	}
 	return (i);
 }
